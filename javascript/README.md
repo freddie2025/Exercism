1b434b886db283720b00238993155412f8da548e
Fred James <fredjsam@live.co.uk>
2020-03-10 12:51:04 +0000

    Resistor Color Duo Challenge Complete

--- javascript/README.md
+++ javascript/README.md
@@ -3,4 +3,5 @@
 ## Completed Challenges
 [Hello World](https://github.com/freddie2025/Exercism/tree/master/javascript/hello-world)  
 [Resistor Color](https://github.com/freddie2025/Exercism/tree/master/javascript/resistor-color)  
+[Resistor Color Duo](https://github.com/freddie2025/Exercism/tree/master/javascript/resistor-color-duo)  
 [Two Fer](https://github.com/freddie2025/Exercism/tree/master/javascript/two-fer)  
---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = (selectedColors) => {
+  return parseInt(COLORS.indexOf(selectedColors[0]) + '' + COLORS.indexOf(selectedColors[1]));
+};
+
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+];
